5fe425d6eec0b7dc556dbf803db62ad0cae92c92
Nathan Smith <nate@nathan@neocortical.net>
2016-01-21 08:03:51 -0800

    add method to verify role

--- charon.go
+++ charon.go
@@ -8,10 +8,12 @@ import (
 type Charon interface {
 	Authenticated(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request)
 	Unauthenticated(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request)
+	HasRole(handler func(http.ResponseWriter, *http.Request), roles ...string) func(http.ResponseWriter, *http.Request)
 }
 
 type middleware struct {
 	isAuthed           func(r *http.Request) bool
+	hasRole            func(r *http.Request, roles ...string) bool
 	failAuthRequired   func(rw http.ResponseWriter, r *http.Request)
 	failAuthProhibited func(rw http.ResponseWriter, r *http.Request)
 }
@@ -20,6 +22,15 @@ type middleware struct {
 func New(isAuthed func(r *http.Request) bool, failAuthRequired func(rw http.ResponseWriter, r *http.Request), failAuthProhibited func(rw http.ResponseWriter, r *http.Request)) Charon {
 	return &middleware{
 		isAuthed:           isAuthed,
+		failAuthRequired:   failAuthRequired,
+		failAuthProhibited: failAuthProhibited,
+	}
+}
+
+func NewWithRoleSupport(isAuthed func(r *http.Request) bool, hasRole func(r *http.Request, roles ...string) bool, failAuthRequired func(rw http.ResponseWriter, r *http.Request), failAuthProhibited func(rw http.ResponseWriter, r *http.Request)) Charon {
+	return &middleware{
+		isAuthed:           isAuthed,
+		hasRole:            hasRole,
 		failAuthRequired:   failAuthRequired,
 		failAuthProhibited: failAuthProhibited,
 	}
@@ -43,6 +54,16 @@ func (m *middleware) Unauthenticated(handler func(http.ResponseWriter, *http.Req
 			handler(rw, r)
 		} else {
 			m.failAuthProhibited(rw, r)
+		}
+	}
+}
+
+func (m *middleware) HasRole(handler func(http.ResponseWriter, *http.Request), roles ...string) func(http.ResponseWriter, *http.Request) {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		if m.isAuthed(r) && m.hasRole(r, roles...) {
+			handler(rw, r)
+		} else {
+			m.failAuthRequired(rw, r)
 		}
 	}
 }
