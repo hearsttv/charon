f797db3db9df9cc0cf401a9e31b7f293b01194b9
Nathan Smith <nsmith@hearst.com>
2016-02-03 09:25:10 -0800

    remove temp debug line

--- charon.go
+++ charon.go
@@ -1,9 +1,6 @@
 package charon
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
 // Charon exposes two wrapper methods for HTTP(S) handlers that enforce auth state one way or the other.
 type Charon interface {
@@ -19,7 +16,6 @@ type middleware struct {
 
 // New creates a new Charon object with the specified callback functions
 func New(isAuthed func(r *http.Request) bool, failAuthRequired func(rw http.ResponseWriter, r *http.Request), failAuthProhibited func(rw http.ResponseWriter, r *http.Request)) Charon {
-	fmt.Printf("charon: %v, %v, %v\n", isAuthed, failAuthRequired, failAuthProhibited)
 	return &middleware{
 		isAuthed:           isAuthed,
 		failAuthRequired:   failAuthRequired,
